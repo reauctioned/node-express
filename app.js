22757e7b7d953a6856b4f493e882ed85bae2c0d1
Saurav <sauravgamer441@gmail.com>
2024-08-11 23:07:51 +0545

    exported a module w/o calling with a variable

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const sayHi = require('./utils')
 // console.log(names);
 // console.log(sayHi);
 const calledPerson = require('./alternative')
-console.log(calledPerson);
+// console.log(calledPerson);
 
-sayHi('saurav')
-sayHi(names.name1)
-sayHi(names.name2)
+// sayHi('saurav')
+// sayHi(names.name1)
+// sayHi(names.name2)
+
+require('./mind-grenade')
 
 
---
+++ mind-grenade.js
@@ -0,0 +1,9 @@
+const a = 5
+const b = 9
+
+function addValues(){
+    console.log(`sum: ${a+b}`);
+    
+}
+
+addValues()

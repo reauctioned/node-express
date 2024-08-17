cd959ec312fe088e5f3af522b323dc4c675809ed
Saurav <sauravgamer441@gmail.com>
2024-08-17 16:53:14 +0545

    built in function for system Uptime in hours

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const os = require('os')
 // const {} = require('os')
 
-const user = os.userInfo()
+const user = os.userInfo()//returns user info
 console.log(user);
+
+const sysUptime = console.log(`Sys Up Time: ${(((os.uptime())/60)/60)} hours`);
+
